9824aa0fea6d23de4a2e55b7d3489fa99c232d85
Suarez Lin <1031312670@qq.com>
2017-02-26 16:02:30 +0800

    fix bug

--- app/main/views.py
+++ app/main/views.py
@@ -115,7 +115,8 @@ def get_blog(id):
         else:
             author = blog.author
             timestamp = utc2local(blog.timestamp).strftime('%Y-%m-%d')
-            return render_template('otherblog.html', author=author, blog=blog, timestamp=timestamp)
+            return render_template('otherblog.html', author=author, blog=blog, timestamp=timestamp,message=message,
+                                   t=timestamp, sig=sig, key=DISQUS_PUBLIC_KEY)
 
 
 @main.route('/delete', methods=['POST'])
